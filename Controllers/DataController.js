b7508fbff63376748cb6c1948775440f5e198c95
Qais99 <87611606+Qais99@users.noreply.github.com>
2021-08-02 18:33:27 +0300

    i

--- Controllers/DataController.js
+++
@@ -1,7 +0,0 @@
-const Data = require('../Shared/Data.json')
-
-exports.getData = (req, res,next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.json(Data);
-}
